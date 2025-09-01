b17280e1e3aacd0dcca10a7ec0d70b5626654e2e
Ewertton Goncalves <dev.ewerttongoncalves@gmail.com>
2025-09-01 06:57:07 -0300

    feat: Adiciona a pagina Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home'
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Cadastro from './pages/cadastro/Cadastro';
+
 
 function App() {
 
   return (
     <>
-      <Home />
+      <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Cadastro />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
   );
 }
